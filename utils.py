62d7d172f05467c433974851334dba86f38c38b7
paintingofblue <90877067+paintingofblue@users.noreply.github.com>
2023-07-16 21:16:16 +1200

    Fix naming of the embed color variable, and add some dogshit documentation for the logger class

--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
+import os
 import requests
 import random
+import json
+from pathlib import Path
 from bs4 import BeautifulSoup
 from datetime import datetime
 from typing import Optional, Union
@@ -7,7 +10,7 @@ from typing import Optional, Union
 # API wrapper for https://thecatapi.com
 class Cat():
     def __init__(self):
-        self.embedColoresponse = 0x3498DB
+        self.embedColor = 0x3498DB
         self.subList = ['IllegallySmolCats', 'cats', 'Catloaf', 'Catswithjobs', 'WhatsWrongWithYourCat', 'blackcats', 'CatSlaps', 'SupermodelCats', 'CatsStandingUp', 'bigcatgifs', 'catbellies', 'catpictures', 'catpranks', 'catsareliquid', 'catsinsinks', 'catsinbusinessattire', 'catswhoyell', 'catswithjobs', 'cattaps', 'catsisuottafo', 'blep', 'fromkittentocat', 'holdmycatnip', 'jellybeantoes', 'kittens', 'kitting', 'kneadycats', 'mainecoons', 'murdermittens', 'nebelung', 'petthedamncat', 'pocketpussy', 'ragdolls', 'startledcats', 'stuffoncats', 'teefies', 'thecattrapisworking', 'tightpussy', 'toebeans', 'tuckedinkitties']
         self.keyList = ['enter-keys-here']
         self.token = 'enter-token-here'
@@ -50,7 +53,21 @@ class Cat():
 
 # Logger
 class Logger:
-    # Initialize colors using ANSI escape codes
+    """# Logger
+    Class used for logging to the console.
+
+    When a function inside of this class is called, it prints using the format of:
+
+    `[+/-/!/*] [time] - [text]`
+
+    ## Methods
+    - `append_to_file` `(path: Union[str, Path], text: str)` - Appends text to a file.
+    - `write_to_file` `(path: Union[str, Path], text: Union[str, dict], *, json_format='pretty')` - Writes text or a dictionary to a file
+    - `info` `(text: str)`
+    - `error` `(text: str)`
+    - `warning` `(text: str)`
+    - `success` `(text: str)`
+    """
     def __init__(self):
         self.red: str = '\033[91m'
         self.yellow: str = '\033[93m'
@@ -58,9 +75,29 @@ class Logger:
         self.grey: str = '\033[90m'
         self.reset: str = '\033[37m'
 
+    def append_to_file(self, path: Union[str, Path], text: str):
+        if not os.path.exists(path):
+            os.makedirs(path, exist_ok=True)
+
+        with open(path, 'a', encoding='utf8') as f:
+            f.write(f'{text}\n')
+
+    def write_to_file(self, path: Union[str, Path], text: Union[str, dict], *, json_format='pretty'):
+        if not os.path.exists(path):
+            os.makedirs(path, exist_ok=True)
+
+        if type(text) == dict:
+            if json_format == 'pretty':
+                text = json.dumps(text, indent=4)
+            else:
+                text = json.dumps(text)
+
+        with open(path, 'w', encoding='utf8') as f:
+            f.write(text)
+
     def info(self, text: str):
         time = datetime.now().strftime("%D %H:%M:%S")
-        print(f'{self.grey}[+]{self.reset} {time} - {text}')
+        print(f'{self.grey}[*]{self.reset} {time} - {text}')
 
     def error(self, text: str):
         time = datetime.now().strftime("%D %H:%M:%S")
