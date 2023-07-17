db62e1c2b625c8917f7eb939d86c161c76ea925a
paintingofblue <90877067+paintingofblue@users.noreply.github.com>
2023-07-17 13:00:39 +1200

    Add a better logging system (optional writing), and add log directory to gitignore

--- utils.py
+++ utils.py
@@ -1,7 +1,6 @@
 import os
 import requests
 import random
-import json
 from pathlib import Path
 from bs4 import BeautifulSoup
 from datetime import datetime
@@ -52,61 +51,98 @@ class Cat():
         return response.json()
 
 # Logger
+import os
+from datetime import datetime
+from pathlib import Path
+
 class Logger:
-    """# Logger
-    Class used for logging to the console.
-
-    When a function inside of this class is called, it prints using the format of:
-
-    `[+/-/!/*] [time] - [text]`
-
-    ## Methods
-    - `append_to_file` `(path: Union[str, Path], text: str)` - Appends text to a file.
-    - `write_to_file` `(path: Union[str, Path], text: Union[str, dict], *, json_format='pretty')` - Writes text or a dictionary to a file
-    - `info` `(text: str)`
-    - `error` `(text: str)`
-    - `warning` `(text: str)`
-    - `success` `(text: str)`
-    """
     def __init__(self):
-        self.red: str = '\033[91m'
-        self.yellow: str = '\033[93m'
-        self.green: str = '\033[92m'
-        self.grey: str = '\033[90m'
-        self.reset: str = '\033[37m'
+        self.red = '\033[91m'
+        self.yellow = '\033[93m'
+        self.green = '\033[92m'
+        self.grey = '\033[90m'
+        self.reset = '\033[37m'
 
-    def append_to_file(self, path: Union[str, Path], text: str):
-        if not os.path.exists(path):
-            os.makedirs(path, exist_ok=True)
+    def append_to_file(self, file_path: str | Path | os.PathLike, text: str | dict, encoding: str='utf8'):
+        path = Path(file_path).parent
+        path.mkdir(parents=True, exist_ok=True)
 
-        with open(path, 'a', encoding='utf8') as f:
-            f.write(f'{text}\n')
+        try:
+            with open(file_path, 'a', encoding=encoding) as f:
+                f.write(f'{text}\n')
+        except PermissionError as e:
+            raise PermissionError(f'Unable to write to {file_path}, as permission was denied.')
+        except OSError as e:
+            raise OSError(f'Unable to write to {file_path}, as the OS returned an error: {e.strerror}')
 
-    def write_to_file(self, path: Union[str, Path], text: Union[str, dict], *, json_format='pretty'):
-        if not os.path.exists(path):
-            os.makedirs(path, exist_ok=True)
+    def fetch_time(self, time: Union[datetime, int, float] = datetime.now(), time_format='now') -> str:
+        allowed_formats = ['now', 'date', 'date-alt', 'full']
+        if time_format not in allowed_formats:
+            time_format = allowed_formats[0]
 
-        if type(text) == dict:
-            if json_format == 'pretty':
-                text = json.dumps(text, indent=4)
-            else:
-                text = json.dumps(text)
+        if type(time) == Union[int, float]:
+            time = datetime.fromtimestamp(time)
 
-        with open(path, 'w', encoding='utf8') as f:
-            f.write(text)
+        match time_format:
+            case 'now':
+                time = datetime.now().strftime('%H:%M:%S')
+            case 'date':
+                time = time.strftime('%d/%m/%Y')
+            case 'date-alt':
+                time = time.strftime('%d-%m-%Y')
+            case 'full':
+                time = time.strftime('%d/%m/%Y %H:%M:%S')
 
-    def info(self, text: str):
-        time = datetime.now().strftime("%D %H:%M:%S")
-        print(f'{self.grey}[*]{self.reset} {time} - {text}')
+        return time
 
-    def error(self, text: str):
-        time = datetime.now().strftime("%D %H:%M:%S")
-        print(f'{self.red}[-]{self.reset} {time} - {text}')
+    def info(self, text: str, write: bool = False):
+        time = datetime.now()
 
-    def warning(self, text: str):
-        time = datetime.now().strftime("%D %H:%M:%S")
-        print(f'{self.yellow}[!]{self.reset} {time} - {text}')
+        console_time = self.fetch_time()
+        console_text = f'[{console_time}] {self.grey}[~]{self.reset} {text}'
+        print(console_text)
 
-    def success(self, text: str):
-        time = datetime.now().strftime("%D %H:%M:%S")
-        print(f'{self.green}[+]{self.reset} {time} - {text}')
+        if write:
+            current_date = self.fetch_time(time=time, time_format='date-alt')
+            file_time = self.fetch_time(time=time)
+            file_text = f'[{file_time}] [INFO] {text}'
+            self.append_to_file(f'logs/{current_date}.log', file_text)
+
+    def success(self, text: str, write: bool = False):
+        time = datetime.now()
+
+        console_time = self.fetch_time()
+        console_text = f'[{console_time}] {self.green}[+]{self.reset} {text}'
+        print(console_text)
+
+        if write:
+            current_date = self.fetch_time(time=time, time_format='date-alt')
+            file_time = self.fetch_time(time=time)
+            file_text = f'[{file_time}] [SUCCESS] {text}'
+            self.append_to_file(f'logs/{current_date}.log', file_text)
+
+    def error(self, text: str, write: bool = False):
+        time = datetime.now()
+
+        console_time = self.fetch_time()
+        console_text = f'[{console_time}] {self.red}[-]{self.reset} {text}'
+        print(console_text)
+
+        if write:
+            current_date = self.fetch_time(time=time, time_format='date-alt')
+            file_time = self.fetch_time(time=time)
+            file_text = f'[{file_time}] [ERROR] {text}'
+            self.append_to_file(f'logs/{current_date}.log', file_text)
+
+    def warning(self, text: str, write: bool = False):
+        time = datetime.now()
+
+        console_time = self.fetch_time()
+        console_text = f'[{console_time}] {self.yellow}[!]{self.reset} {text}'
+        print(console_text)
+
+        if write:
+            current_date = self.fetch_time(time=time, time_format='date-alt')
+            file_time = self.fetch_time(time=time)
+            file_text = f'[{file_time}] [WARNING] {text}'
+            self.append_to_file(f'logs/{current_date}.log', file_text)
